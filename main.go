33e69345c28cc11b3e9ae807a66b76f3343f3214
Rizanoor <rizanoorfauzan123@gmail.com>
2023-01-09 10:52:05 +0700

    Routing Request

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/mario", marioHandler)
 
 	log.Println("Starting web on port 8080")
 
@@ -16,6 +18,16 @@ func main() {
 	log.Fatal(err)
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello ini halaman home"))
+}
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world, saya sedang belajar golang"))
+}
+func marioHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello mario, saya sedang belajar golang"))
 }
